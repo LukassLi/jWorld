14ded58963a0487376adea19091add1d4b52b129
LukassLi <598979698@qq.com>
2022-04-21 00:00:18 +0800

    feat: 用户登录服务端

---
+++ Src/Server/GameServer/GameServer/Services/UserService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Common;
+using Network;
+using SkillBridge.Message;
+
+namespace GameServer.Services
+{
+    class UserService: Singleton<UserService>
+    {
+        public UserService()
+        {
+            MessageDistributer<NetConnection<NetSession>>.Instance.Subscribe<UserLoginRequest>(this.OnLogin);
+
+
+
+
+        }
+
+        private void OnLogin(NetConnection<NetSession> sender, UserLoginRequest request)
+        {
+            Log.InfoFormat("UserLoginRequest: User:{0}  Pass:{1}", request.User, request.Passward);
+
+            NetMessage message = new NetMessage();
+            message.Response = new NetMessageResponse();
+            message.Response.userLogin = new UserLoginResponse();
+
+            TUser user = DBService.Instance.Entities.Users.Where(u => u.Username == request.User).FirstOrDefault();
+            if(user == null)
+            {
+                message.Response.userLogin.Result = Result.Failed;
+                message.Response.userLogin.Errormsg = "";
+            }
+            else if(user.Password!=user.Password)
+            {
+                message.Response.userLogin.Result = Result.Failed;
+            }
+            
+        }
+    }
+}
